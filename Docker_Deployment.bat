1b3ad524b21ad1a79fed21e7e2509f4506a98f6b
Nivetha-vijay <Nivethavijay07@gmail.com>
2024-12-09 14:03:16 +0530

    Update Docker_Deployment.bat

--- Docker_Deployment.bat
+++ Docker_Deployment.bat
@@ -1,13 +1,15 @@
 @ECHO off
 Title "Docker Deployment"
-cd D:\QA\ZIEWEB_Container_v1.0\Build\
+CALL %Script%\Config_Data.bat
+CD %BuildPath%\
+
 set /p Build=Enter the Build Number : 
 Set date=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 set time=%TIME:~6,2%%TIME:~3,2%%TIME:~0,2%
 set dateandtime=%date%_%time: =0%
 mkdir %Build%_%dateandtime%
 ECHO "%Build%_%dateandtime% File created"
-CALL D:\QA\ZIEWEB_Container_v1.0\Build\Script\Config_Data.bat
+CALL %Script%\Config_Data.bat
 CD %LocalLocation%
 ECHO "Copy the Build from Build machine to local"
 COPY %BuildLocation% %LocalLocation%
@@ -28,33 +30,33 @@ FOR /F %%G IN (Images.txt) DO (
 set A=%%G
 ECHO !A! | findstr "%LICENSE%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e securedConnection=%securedConnection% -p %LM_port_http% -p %LM_port_https% -v "%LM_Mount%" --name %LMNAME% !A!
+    docker run -d -it --network=%Build%_%dateandtime% -e securedConnection=%securedConnection% -p %LM_port_http%:%LM_port_http% -p %LM_port_https%:%LM_port_https% -v "%LM_Mount%" --name %LMNAME% !A!
 	ECHO %LMNAME% container created sucessfully
 	)
 ECHO !A! | findstr "%REDIRECT%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e ACCEPT_LICENSE=%ACCEPT_LICENSE% -p "%Redirector_Port%" -v "%Redirector_Cert%" --name %Redirector% !A!
+    docker run -d -it --network=%Build%_%dateandtime% -e ACCEPT_LICENSE=%ACCEPT_LICENSE% -p "%Redirector_Port%:%Redirector_Port%" -v "%Redirector_Cert%" --name %Redirector% !A!
 	ECHO %Redirector% container created sucessfully
 	)
 ECHO !A! | findstr "%FTP%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e ENVIRONMENT=%ENVIRONMENT% -p %FTP_Port% -v "%FTP_Mount%" -v "%VTFTP_Cert_Mount%" --name %FTPCONTAINERNAME% !A!
+    docker run -d -it --network=%Build%_%dateandtime% -e ENVIRONMENT=%ENVIRONMENT% -p %FTP_Port%:%FTP_Port% -v "%FTP_Mount%" -v "%VTFTP_Cert_Mount%" --name %FTPCONTAINERNAME% !A!
 	ECHO %FTPCONTAINERNAME% container created sucessfully
 	)
 ECHO !A! | findstr "%VT%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e ENVIRONMENT=%ENVIRONMENT% -p %VT_Port% -v "%VT_Mount%" -v "%VTFTP_Cert_Mount%" --name %VTCONTAINERNAME% !A!
+    docker run -d -it --network=%Build%_%dateandtime% -e ENVIRONMENT=%ENVIRONMENT% -p %VT_Port%:%VT_Port% -v "%VT_Mount%" -v "%VTFTP_Cert_Mount%" --name %VTCONTAINERNAME% !A!
 	ECHO %VTCONTAINERNAME% container created sucessfully
 	)
 ECHO !A! | findstr "%CLIENT%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e ZIEWebServer=%ZIEWebServer% -e LMName=%LMNAME% -e securedConnection=%securedConnection% -e DEPENV=%DEPENV% -e FTPCONTAINERNAME=%FTPCONTAINERNAME% -e VTCONTAINERNAME=%VTCONTAINERNAME% -e HOSTFORDOCKER=%HOSTFORDOCKER% -e SOCKERPATH=%SOCKERPATH% -p %APP_Http_Port% -p %APP_Https_Port% -v %Client_Mount% --name %ziewebclient% !A!
-	ECHO %ziewebclient% container created sucessfully
+    docker run -d -it --network=%Build%_%dateandtime% -e %Server_ENV%=%WebServer% -e LMName=%LMNAME% -e securedConnection=%securedConnection% -e DEPENV=%DEPENV% -e FTPCONTAINERNAME=%FTPCONTAINERNAME% -e VTCONTAINERNAME=%VTCONTAINERNAME% -e HOSTFORDOCKER=%HOSTFORDOCKER% -e SOCKERPATH=%SOCKERPATH% -p %APP_Http_Port%:%APP_Http_Port% -p %APP_Https_Port%:%APP_Https_Port% -v %Client_Mount% --name %WebClient% !A!
+	ECHO %WebClient% container created sucessfully
 	)
 ECHO !A! | findstr "%SERVER%" > nul 2>&1
 if not errorlevel 1 (
-    docker run -d -it --network=%Build%_%dateandtime% -e ACCEPT_LICENSE=%ACCEPT_LICENSE% -e ZIEWebServer=%ZIEWebServer% -p %ZIEWEB_Http_Port% -p %ZIEWEB_Https_Port% -p %ZIEWEB_config_Port% -v "%Bin_Cert%" -v "%ZIEWEB_Cert%" -v "%Private_Mount%" -v "%DW_Mount%" -v "%Migration_Mount%" --name %ZIEWebServer% !A!
-	ECHO %ZIEWebServer% container created sucessfully
+    docker run -d -it --network=%Build%_%dateandtime% -e ACCEPT_LICENSE=%ACCEPT_LICENSE% -e %Server_ENV%=%WebServer% -p %Server_Http_Port%:%Server_Http_Port% -p %Server_Https_Port%:%Server_Https_Port% -p %Server_config_Port%:%Server_config_Port% -v "%Bin_Cert%" -v "%Publish_Cert%" -v "%Private_Mount%" -v "%DW_Mount%" -v "%Migration_Mount%" --name %WebServer% !A!
+	ECHO %WebServer% container created sucessfully
 	)
 	)
 docker ps
@@ -70,21 +72,21 @@ mkdir logs
 CD %logsLocation%
 docker logs %FTPCONTAINERNAME% >> %FTPCONTAINERNAME%_logs.txt
 docker logs %VTCONTAINERNAME% >> %VTCONTAINERNAME%_logs.txt
-docker logs %ziewebclient% >> %ziewebclient%_logs.txt
-docker logs %ZIEWebServer% >> %ZIEWebServer%_logs.txt
+docker logs %WebClient% >> %WebClient%_logs.txt
+docker logs %WebServer% >> %WebServer%_logs.txt
 docker logs %LMNAME% >> %LMNAME%_logs.txt
 docker logs %Redirector% >> %Redirector%_logs.txt
 
-start http://localhost:9080/zieweb/adminconsole
-start http://localhost:8080/zie/
-start http://localhost:9088/LicenseManager/LicenseLogger
+start http://localhost:%APP_Http_Port%/%WebAlias%/%Adminconsole%
+start http://localhost:%Server_Http_Port%/%ServerAlias%/%AdminFull%
+start http://localhost:%LM_port_http%/%LmAlias%/%LicenseLogger%
 
 set /p userinput=Do you Want to Execute Feature Testing :
 if %userinput%==y goto Feature
 if %userinput%==n goto cmd
 
 :Feature
-CALL D:\QA\ZIEWEB_Container_v1.0\Features\FeatureFile.bat
+CALL %FeatureFile%\FeatureFile.bat
 
 :cmd
 ECHO Terminal will exit in 10 Secs
